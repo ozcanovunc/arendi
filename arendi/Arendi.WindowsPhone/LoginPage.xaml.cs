c2f0775437e9fc86fd7d46f01a4b1a87e80bda26
ozcanovunc <ozcan_ovunc@hotmail.com>
2015-10-21 12:09:08 +0300

    Implemented HubPage

---
+++ arendi/Arendi.WindowsPhone/LoginPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Windows.Phone.UI.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Navigation;
+
+namespace Arendi
+{
+    public sealed partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            HardwareButtons.BackPressed += HardwareButtons_BackPressed;
+        }
+
+        protected override void OnNavigatingFrom(NavigatingCancelEventArgs e)
+        {
+            base.OnNavigatingFrom(e);
+            HardwareButtons.BackPressed -= HardwareButtons_BackPressed;
+        }
+
+        private void HardwareButtons_BackPressed(object sender, BackPressedEventArgs e)
+        {
+            // TODO: Are you sure you want to exit?
+            Application.Current.Exit();
+        }
+
+        private void MainPage_LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            // TODO: Login mechanism
+            Frame.Navigate(typeof(HubPage));
+        }
+    }
+}
